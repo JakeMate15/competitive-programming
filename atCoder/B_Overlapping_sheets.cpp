41137ed2a90f5d6c73c4d324328f2284baf653f2
JakeMate15 <erik.mofe51@gmail.com>
2023-09-12 08:28:04 -0600

    mas problemas

--- atCoder/B_Overlapping_sheets.cpp
+++ atCoder/B_Overlapping_sheets.cpp
@@ -15,48 +15,31 @@ typedef long long ll;
 typedef vector<int> vi;
 typedef pair<int,int> ii;
 
-long long area(int bl_x, int bl_y, int tr_x, int tr_y) {
-	long long length = tr_y - bl_y;
-	long long width = tr_x - bl_x;
-	return length * width;
-}
-
-int inter_area(vector<int> s1, vector<int> s2) {
-	int bl_a_x = s1[0], bl_a_y = s1[1], tr_a_x = s1[2], tr_a_y = s1[3];
-	int bl_b_x = s2[0], bl_b_y = s2[1], tr_b_x = s2[2], tr_b_y = s2[3];
-
-	return ((min(tr_a_x, tr_b_x) - max(bl_a_x, bl_b_x)) *
-	        (min(tr_a_y, tr_b_y) - max(bl_a_y, bl_b_y)));
-}
+int g[100][100];
 
 void sol(){
     int n;
     cin>>n;
 
-    ll ar = 0;
-    vector<vector<int>> rectangulos(n, vector<int>(2));
     forn(i,n){
         int a,b,c,d;
         cin>>a>>b>>c>>d;
 
-        rectangulos[i][0] = a;
-        rectangulos[i][1] = c;
-        rectangulos[i][2] = b;
-        rectangulos[i][3] = d;
-
-        ar+=area(a,c,b,d);
-    }
-
-    forn(i,n){
-        forn(j,n){
-            if(j==i)    continue;
-            debln(inter_area(rectangulos[i],rectangulos[j]));
-            ar-=inter_area(rectangulos[i],rectangulos[j]);
+        forr(j,a,b){
+            forr(k,c,d){
+                g[j][k] = 1;
+            }
         }
     }
 
-    debln(ar);
+    int res = 0;
+    forn(i,100){
+        forn(j,100){
+            if(g[i][j]) res++;
+        }
+    }
 
+    debln(res);
 }
 
 int main(){
--- codeForces/H_2_Maximum_Crossings_Hard_Version.cpp
+++ codeForces/H_2_Maximum_Crossings_Hard_Version.cpp
@@ -1,27 +1,83 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define IO  ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define forn(i,n)   for(int (i)=0; i<n; i++)
-#define forr(i,a,n) for(int i=(a); i<n; i++)
-#define fore(i,a,n) for(int i=(a); i<=n; i++)
-#define all(v)	    v.begin(),v.end()
-#define borra(s)    s.erase(unique(all(s)),s.end())
-#define yes         cout << "YES\n"
-#define no          cout << "NO\n"
-#define debug(a)    cout << a << "\n"
-#define sz(a)       (int)a.size()
+#define forn(i,n)       for(int i=0; i<n; i++)
+#define forr(i,a,n)     for(int i=a; i<n; i++)
+#define fore(i,a,n)     for(int i=a; i<=n; i++)
+#define fornd(i,n)      for(int i=n; i>=0; i--)
+#define all(v)          v.begin(),v.end()
+#define sz(a)           (int)a.size()
+#define debln(a)        cout << a << "\n"
+#define deb(a)          cout << a << " "
+#define pb              push_back
 
 typedef long long ll;
 typedef vector<int> vi;
-typedef pair<int,int> pii;
+typedef pair<int,int> ii;
+
+const int mx = 2e5+5;
+template<typename T>
+struct FenwickTree{
+	int N;
+	vector<T> bit;
+
+	//build from array in O(n), indexed in 0
+	FenwickTree(int N): N(N){
+		bit.resize(N);
+	}
+
+	//single element increment
+	void update(int pos, T value){
+		while(pos < N){
+			bit[pos] += value;
+			pos |= pos + 1;
+		}
+	}
+
+	//range query, [0, r]
+	T query(int r){
+		T res = 0;
+		while(r >= 0){
+			res += bit[r];
+			r = (r & (r + 1)) - 1;
+		}
+		return res;
+	}
+
+	//range query, [l, r]
+	T query(int l, int r){
+		return query(r) - query(l - 1);
+	}
+};
 
 void sol(){
-	
+	int n;
+	cin>>n;
+
+	vi a(n);
+	for(auto &x: a){
+		cin>>x;
+	}
+
+	FenwickTree<int> bit(mx);
+	ll res = 0;
+
+	forn(i,n){
+		res+=bit.query(a[i],mx-3);
+		bit.update(a[i],1);
+	}
+
+	debln(res);
 }
 
-int main(){IO
+int main(){
+	ios::sync_with_stdio(false);cin.tie(0);
+
 	int t=1;
 	cin>>t;
-	while(t--)  sol();
+	while(t--){
+		sol();
+	}
+
+	return 0;
 }
