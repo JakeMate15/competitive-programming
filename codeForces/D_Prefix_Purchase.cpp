a020807af3f3a65f2bba34bb259e27499c6b978f
JakeMate15 <erik.mofe51@gmail.com>
2023-10-02 22:25:43 -0600

    nc p2

---
+++ codeForces/D_Prefix_Purchase.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif 
+#define all(v)          v.begin(),v.end()
+#define sz(a)           (int)a.size()
+#define debln(a)        cout << a << "\n"
+#define deb(a)          cout << a << " "
+#define nl              cout << "\n";
+typedef long long ll;
+
+//Podemos verificar que siempre es mejor comprar un valor mas grande por menos 
+
+void sol(){
+    int n, k;
+    cin >> n;
+
+    vector<int> c(n);
+    for(int i = 0; i < n; i++) {
+        cin >> c[i];
+    }
+    debug(c);
+    for(int i = n - 2; i >= 0; i--) {
+        c[i] = min(c[i], c[i + 1]);
+    }
+    debug(c);
+
+    cin >> k;
+    for(int i = 0; i < n; i++) {
+       deb(k / c[i]); 
+    }
+
+    nl
+}
+
+int main(){
+    ios::sync_with_stdio(false);cin.tie(0);
+
+    int t=1;
+    cin>>t;
+    while(t--){
+        sol();
+    }
+
+    return 0;
+}
