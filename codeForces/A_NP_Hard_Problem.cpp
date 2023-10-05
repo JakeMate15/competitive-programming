7c73afebc637b026586673ab521ec26861084b79
JakeMate15 <erik.mofe51@gmail.com>
2023-10-04 22:34:56 -0600

    nc

--- codeForces/A_NP_Hard_Problem.cpp
+++ codeForces/A_NP_Hard_Problem.cpp
@@ -8,62 +8,75 @@ using namespace std;
 #define nl              cout << "\n";
 typedef long long ll;
 
-int isBipartiteDFS(int current, vector<vector<int>> &graph, vector<int> &color){
-	bool is = 1;
-	for(int child : graph[current]){
-		if(color[child] == 0){//no esta coloreado
-			color[child] = color[current] == 1 ? 2 : 1;// coloreo con el color opuesto a mi nodo current
-			is = is & isBipartiteDFS(child, graph, color);
-		}
-		else if(color[child] == color[current]) return 0;//si son iguales no es bipartito 
-	}
-	return is;
+vector<vector<int>> graph;
+vector<int> side;
+ 
+bool isBipartite(int n){
+    bool isBipartite = true;
+    side.resize(n + 1, -1);
+    queue<int> q;
+    for(int st = 0; st<=n; st++){
+        if(side[st] != -1) continue;
+        q.push(st);
+        side[st] = 0;
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
+            for(int v : graph[u]){
+                if(side[v] == -1){
+                    q.push(v);
+                    side[v] = side[u]^1;
+                } else {
+                    isBipartite &= side[v] != side[u];
+                }
+            }
+        }
+    }
+    return isBipartite;
 }
+ 
 
 void sol(){
     int n, m;
     cin >> n >> m;
 
-    vector<vector<int>> adj;
+    graph.resize(n + 1);
+
     for(int i = 1; i <= m; i++) {
         int u, v;
         cin >> u >> v;
-        adj[u].push_back(v);
+        graph[u].push_back(v);
+        graph[v].push_back(u);
     }
 
-    vector<int> color(n + 1);
-
-    if(isBipartiteDFS(1, adj, color) == 0) {
+    if(!isBipartite(n)) {
         debln(-1);
-        return;
     }
-
-    for(auto x: color) {
-        deb(x);
-    }
-    nl;
-
-    vector<int> g1, g2;
-    for(int i = 1; i <= n; i++) {
-        if(color[i] == 1) {
-            g1.push_back(i);
+    else{
+        
+        vector<int> c1, c2;
+        for(int i = 1; i <= n; i++) {
+            if(side[i] == 0) {
+                c1.push_back(i);
+            }
+            else if(side[i] == 1) {
+                c2.push_back(i);
+            }
         }
-        else if(color[i] == 0){
-            g2.push_back(i);
+
+        debln(sz(c1));
+        for(auto x: c1) {
+            deb(x);
         }
-    }
+        nl;
 
-    debln(sz(g1));
-    for(auto x: g1) {
-        deb(x);
+        debln(sz(c2));
+        for(auto x: c2) {
+            deb(x);
+        }
+        nl;
     }
-    nl;
-
-    debln(sz(g2));
-    for(auto x: g2) {
-        deb(x);
-    }
-    nl;
+    
 }
 
 int main(){
--- codeForces/D_Prefix_Purchase.cpp
+++ codeForces/D_Prefix_Purchase.cpp
@@ -1,32 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void __print(int x) {cerr << x;}
-void __print(long x) {cerr << x;}
-void __print(long long x) {cerr << x;}
-void __print(unsigned x) {cerr << x;}
-void __print(unsigned long x) {cerr << x;}
-void __print(unsigned long long x) {cerr << x;}
-void __print(float x) {cerr << x;}
-void __print(double x) {cerr << x;}
-void __print(long double x) {cerr << x;}
-void __print(char x) {cerr << '\'' << x << '\'';}
-void __print(const char *x) {cerr << '\"' << x << '\"';}
-void __print(const string &x) {cerr << '\"' << x << '\"';}
-void __print(bool x) {cerr << (x ? "true" : "false");}
-
-template<typename T, typename V>
-void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
-template<typename T>
-void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
-void _print() {cerr << "]\n";}
-template <typename T, typename... V>
-void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
-#ifndef ONLINE_JUDGE
-#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
-#else
-#define debug(x...)
-#endif 
 #define all(v)          v.begin(),v.end()
 #define sz(a)           (int)a.size()
 #define debln(a)        cout << a << "\n"
@@ -34,7 +8,9 @@ void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v..
 #define nl              cout << "\n";
 typedef long long ll;
 
-//Podemos verificar que siempre es mejor comprar un valor mas grande por menos 
+//Podemos verificar que siempre es mejor comprar un valor mas grande por menos
+//Siempre es mejor tomar el valor mas alto posible con la i menor, despues podemos reviasar si sobra dinero
+//Podemos construir un arreglo creciente donde a su vez podemos actualizar el valor para cada pos i
 
 void sol(){
     int n, k;
@@ -44,18 +20,25 @@ void sol(){
     for(int i = 0; i < n; i++) {
         cin >> c[i];
     }
-    debug(c);
+
     for(int i = n - 2; i >= 0; i--) {
         c[i] = min(c[i], c[i + 1]);
     }
-    debug(c);
 
     cin >> k;
+    
+    int t = k;
     for(int i = 0; i < n; i++) {
-       deb(k / c[i]); 
-    }
+        //Cuantas monedas mas me cuesta actualizar hasta aqui
+        int add = c[i] - (i == 0 ? 0 : c[i - 1]);
+        //Si no cuesta, entonces simplemente puedo actualizar al mismo valor
+        //Si cuesta algo, debo de ver si es posible tomar el siguiente 
+        t = min(t, add == 0 ? t : k / add);
+        k -= add * t;
 
-    nl
+        deb(t);
+    }
+    nl;
 }
 
 int main(){
