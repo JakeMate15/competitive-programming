65066a4bc9700090a4efd5ec02fcd1042e911bfe
JakeMate15 <erik.mofe51@gmail.com>
2024-02-25 19:43:13 -0600

    atcoder

---
+++ atCoder/B_Permutation_Check.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define all(v)          v.begin(),v.end()
+#define sz(a)           (int)a.size()
+#define nl              cout << "\n";
+
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T> using ordered_multi_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+
+const int mod = 1e9 + 7;
+const int MX = 2e5 + 5;
+
+void sol() {
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for (auto &x: a) {
+        cin >> x;
+    }
+    sort(all(a));
+
+    for (int i = 0; i < n; i++) {
+        if (a[i] != i + 1) {
+            cout << "No\n";
+            return;
+        }
+    }
+
+    cout << "Yes\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    // cout << fixed << setprecision(10);
+
+    int t = 1;
+    //cin >> t;
+
+    while(t--) {
+        sol();
+    }
+
+    return 0;
+}
---
+++ atCoder/C_Long_Sequence.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define all(v)          v.begin(),v.end()
+#define sz(a)           (int)a.size()
+#define nl              cout << "\n";
+
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T> using ordered_multi_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+
+const int mod = 1e9 + 7;
+const int MX = 2e5 + 5;
+
+void sol() {
+    int n;
+    cin >> n;
+
+    ll sum = 0, x;
+    vector<ll> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        sum += a[i];
+    }
+    cin >> x;
+    
+    ll veces = x / sum;
+    ll res = veces * n;
+    ll curr = veces * sum;
+
+    for (int i = 0; i < n; i++) {
+        if (curr > x) {
+            break;
+        }
+        res++;
+        curr += a[i];
+    }
+
+    cout << res << "\n";
+} 
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    // cout << fixed << setprecision(10);
+
+    int t = 1;
+    //cin >> t;
+
+    while(t--) {
+        sol();
+    }
+
+    return 0;
+}
