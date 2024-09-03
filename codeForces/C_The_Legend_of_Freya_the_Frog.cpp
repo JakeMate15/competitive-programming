fb3fc411d0e443b71f061312b13075590b66c0cd
JakeMate15 <erik.mofe51@gmail.com>
2024-09-03 11:19:04 -0600

    upd

---
+++ codeForces/C_The_Legend_of_Freya_the_Frog.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+    #include "algoDebug.h"
+#else
+    #define debug(x...)
+    #define RAYA 
+    #define raya 
+#endif
+
+#define sz(a)       (int) a.size()
+#define all(a)      a.begin(), a.end()
+#define rall(a)     a.rbegin(), a.rend()
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> ii;
+
+const int MX = 2E5 + 5;
+const int MOD = 1E9 + 7;
+
+void sol () {
+    ll x, y, k;
+    cin >> x >> y >> k;
+
+    ll mnX = (x + k - 1) / k; 
+    ll mnY = (y + k - 1) / k;
+
+    ll n = max(mnX * 2 - 1, mnY * 2); 
+
+    cout << n << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    // cout << fixed << setprecision(10);
+
+    int t = 1;
+    cin >> t;
+
+    while(t--) {
+        sol();
+    }
+
+    return 0;
+}
